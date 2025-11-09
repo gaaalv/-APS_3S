b4bdc79c15653269e9a790a76e3251a2fec30f3b
Gabriel Alves <96218701+gaaalv@users.noreply.github.com>
2025-11-09 02:11:40 -0300

    comentarios
    Added validation for command line arguments and file creation.

--- gerador.c
+++ gerador.c
@@ -3,23 +3,26 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-	
+
+	//validação para receber o número correto de parâmetros, <nome do exe> <quantidade> <nome_arquivo>.
 	if(argc != 3) {
 		printf("Quantidade de argumentos insuficiente!\n");
 		printf("Uso Correto: %s	<quantidade> <nome_do_arquivo>", argv[0]);
 		return 1;
 	}
-		
+
 	int qty = atoi(argv[1]);
 	char* nome_arquivo = argv[2];
-	
+
+	//validação quantidade > 0.
 	if (qty <= 0) {
 		printf("A quantidade nao pode ser igual/inferior a 0!\n");
 		return 1;
 	}
 	
 	FILE *arquivo = fopen(nome_arquivo, "w+");
-	
+
+	//validação caso arquivo não seja encontrado, caso não seja limpar memória.
 	if(arquivo == NULL){
 		printf("Erro ao criar o arquivo solicitado!\n");
 		return 1;
@@ -28,6 +31,7 @@ int main(int argc, char *argv[]) {
 
 	srand(time(NULL));
 
+	//loop para inserir dados aleatorios no arquivo sendo gerado
 	for(int i = 0; i < qty; i++) {
 		int dado_aleatorio = rand();
 		fprintf(arquivo, "%d\n", dado_aleatorio);
